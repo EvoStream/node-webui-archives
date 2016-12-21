36091664893a94d9b3f2596044463327c1da2350
toni08 <tpanganiban@evostream.com>
2016-12-21 14:34:30 +0800

    Added installers and tarballs to the archive

---
+++ installers/web_ui-0.0.1-sunos-x64.sh
@@ -0,0 +1,45 @@
+﻿#!/bin/sh
+#
+# Script to install node.js, web_ui, and then start ERS.
+#
+
+# Get the needed parameters
+OS=sunos
+ARCH=x64
+VER=4.1.1
+NODE_JS=http://nodejs.org/dist/v$VER/node-v$VER-$OS-$ARCH.tar.gz
+
+# Download and extract needed node JS
+which node
+if [ $? != 0 ]; then 
+    echo "Node.js DOES NOT exists.... Installing Node.js first...." 
+
+    wget $NODE_JS 
+
+    # Next, execute the following command to install the Node.js binary package in /usr/local/:
+    tar zxf node-v$VER-$OS-$ARCH.tar.gz -C /usr/local --strip-components 1 
+    if [ $? != 0 ]; then echo 'Node.js: INSTALLATION FAILED! Please see errors below:'; exit 1; fi
+	
+	echo "Node.js: SUCCESSFUL INSTALLATION!" 
+
+    
+else
+    echo "Node.js already exists" 
+fi 
+
+# Install dependencies
+echo 'Installing NPM web_ui...'
+npm install --save https://github.com/EvoStream/node-webui/tarball/develop
+if [ $? != 0 ]; then echo 'web_ui: INSTALLATION FAILED! Please see errors below:'; exit 1; fi
+
+echo "web_ui: SUCCESSFUL INSTALLATION!" 
+
+# Change to where evowebservices is
+cd node_modules/web_ui
+
+# Start web_ui
+echo 'Starting web_ui...'
+npm start 
+if [ $? != 0 ]; then echo 'Could not start web_ui!'; exit 1; fi
+
+exit 0;
---
+++ installers/web_ui-0.0.1-win-x64.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+
+echo creating installation folder in node_webui\.... 
+IF NOT EXIST node_webui MD node_webui
+
+echo changing directory to node_webui\......
+CD node_webui
+
+echo getting evowebservices on github...
+call npm install --save https://github.com/EvoStream/node-webui/tarball/develop
+
+echo changing directory to evowebservices....
+cd node_modules\web_ui
+
+echo starting evowebservices using npm....
+call npm start
+
+echo.
+echo EVOWEBSERVICES INSTALLED ON %cd%\node_webui\node_modules\web_ui
+echo PRESS ANY KEY TO EXIT. 
+echo.
+pause >nul
